8549b97a274cc0da8b71c339433b838dc2736f78
Alexandre Reis <dreanmer@gmail.com>
2021-06-08 13:02:58 -0300

    improve credentials usage

--- src/HttpClient/GuzzleHttpClient.php
+++ src/HttpClient/GuzzleHttpClient.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace CIO\HttpClient;
 
+use CIO\Entity\AuthType;
 use CIO\Entity\RequestMethod;
 use CIO\Exception\InvalidCredentials;
+use CIO\Exception\NotImplemented;
 use CIO\Response\BaseResponse;
 use CIO\Response\CustomerIoResponse;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 class GuzzleHttpClient implements HttpClientInterface
 {
@@ -18,47 +21,95 @@ class GuzzleHttpClient implements HttpClientInterface
      * @var \GuzzleHttp\Client
      */
     private $guzzleClient;
+    /**
+     * @var mixed[]
+     */
+    private $credentials;
 
     /**
      * GuzzleHttpClient constructor.
      *
      * @param mixed[] $credentials
-     *
-     * @throws \CIO\Exception\InvalidCredentials
      */
     public function __construct(array $credentials)
     {
-        $config = [];
-
-        if (isset($credentials['site_id']) && isset($credentials['api_key'])) {
-            $config['auth'] = [
-                $credentials['site_id'],
-                $credentials['api_key'],
-            ];
-        } else {
-            if (isset($credentials['token'])) {
-                $config['headers'] = [
-                    'Authorization' => sprintf('Bearer %s', $credentials['token']),
-                ];
-            } else {
-                throw new InvalidCredentials('site_id and api_key or token is needed to authenticate on customer.io, please input a valid one.');
-            }
-        }
-
-        $this->guzzleClient = new Client($config
-        );
+        $this->guzzleClient = new Client();
+        $this->credentials  = $credentials;
     }
 
-    public function request(RequestMethod $method, string $uri, array $body = []) : CustomerIoResponse
-    {
-        $response = $this->guzzleClient->request(
-            $method->getValue(),
-            self::PROTOCOL . $uri,
-            [
-                'json' => $body,
-            ]
-        );
+    /**
+     * @throws \CIO\Exception\InvalidCredentials
+     * @throws \CIO\Exception\NotImplemented
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function request(
+        RequestMethod $method,
+        string $uri,
+        AuthType $authType,
+        array $body = []
+    ) : CustomerIoResponse {
+        switch ($authType->getValue()) {
+            case AuthType::BASIC:
+                $options = $this->getBasicAuth();
+                break;
+            case AuthType::BEARER:
+                $options = $this->getBearerAuth();
+                break;
+            default:
+                throw new NotImplemented('credentials type not implemented');
+        }
 
-        return new BaseResponse($response->getBody()->getContents());
+        $options['json'] = $body;
+
+        try {
+            $response = $this->guzzleClient->request(
+                $method->getValue(),
+                self::PROTOCOL . $uri,
+                $options
+            );
+        } catch (RequestException $e) {
+            if (is_null($e->getResponse())) {
+                throw $e;
+            }
+
+            $response = $e->getResponse();
+        }
+
+        return new BaseResponse($response->getStatusCode(), $response->getBody()->getContents());
+    }
+
+    /**
+     * @return mixed[]
+     * @throws \CIO\Exception\InvalidCredentials
+     */
+    private function getBasicAuth() : array
+    {
+        if (!isset($this->credentials['site_id']) || !isset($this->credentials['api_key'])) {
+            throw new InvalidCredentials('this request need site_id and api_key credentials');
+        }
+
+        return [
+            'auth' => [
+                $this->credentials['site_id'],
+                $this->credentials['api_key'],
+            ],
+        ];
+    }
+
+    /**
+     * @return mixed[]
+     * @throws \CIO\Exception\InvalidCredentials
+     */
+    private function getBearerAuth() : array
+    {
+        if (!isset($this->credentials['token'])) {
+            throw new InvalidCredentials('this request need site_id and api_key credentials');
+        }
+
+        return [
+            'headers' => [
+                'Authorization' => sprintf('Bearer %s', $this->credentials['token']),
+            ],
+        ];
     }
 }
--- src/Response/BaseResponse.php
+++ src/Response/BaseResponse.php
@@ -11,15 +11,25 @@ class BaseResponse implements CustomerIoResponse
      * @var string
      */
     private $rawBody;
+    /**
+     * @var int
+     */
+    private $statusCode;
 
-    public function __construct(string $rawBody)
+    public function __construct(int $statusCode, string $rawBody)
     {
-        $this->rawBody = $rawBody;
+        $this->rawBody    = $rawBody;
+        $this->statusCode = $statusCode;
     }
 
     public function getRawBody() : string
     {
         return $this->rawBody;
+    }
+
+    public function getStatusCode() : int
+    {
+        return $this->statusCode;
     }
 
     public function getDecodedBody() : array
