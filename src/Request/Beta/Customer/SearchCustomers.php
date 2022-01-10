0134db59efafdd221359acd0f40b6ba3ddd25ae1
Alexandre Reis <dreanmer@gmail.com>
2022-01-10 15:50:25 -0300

    add search for customers api method

---
+++ src/Request/Beta/Customer/SearchCustomers.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CIO\Request\Beta\Customer;
+
+use CIO\Entity\RequestMethod;
+use CIO\Request\Beta\BetaBaseRequest;
+
+class SearchCustomers extends BetaBaseRequest
+{
+    /**
+     * @var string
+     */
+    private $filters;
+
+    /**
+     * @param string $filters - to understand the filters array: https://customer.io/docs/api/#operation/getPeopleFilter
+     */
+    public function __construct(string $filters)
+    {
+        $this->filters = $filters;
+    }
+
+    public function getEndpoint() : string
+    {
+        return '/v1/api/customers';
+    }
+
+    public function getMethod() : RequestMethod
+    {
+        return RequestMethod::POST();
+    }
+
+    public function getBody() : array
+    {
+        return [
+            'filter' => $this->filters
+        ];
+    }
+}
